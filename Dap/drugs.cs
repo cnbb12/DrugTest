bb12c2f44a3c24218a48b8b3316b2af9ac45443b
cnbb12 <957971829@qq.com>
2019-02-17 10:24:40 +0800

    实现个推

--- Dap/drugs.cs
+++ Dap/drugs.cs
@@ -19,7 +19,7 @@ namespace Dap
         /// <param name="ownerId">上传者userId</param>
         /// <param name="page">查询第几页（默认每页有8个数据）</param>
         /// <returns></returns>
-        public static List<Models.Drugs> getDrugsList(string status,string ownerId,int page)
+        public static List<Models.Drugs> getDrugsList(string status)
         {
             var list_drugs = new List<Models.Drugs>();
             using (DataContext dc = new DataContext(common.conn))
@@ -27,7 +27,7 @@ namespace Dap
                 if ("1".Equals(status))//已过期
                 {
                     var _list_drugs = from x in dc.GetTable<Drugs>()
-                                     where x.OwnerId.ToString() == ownerId && x.RemainDay <= 0
+                                     where x.RemainDay == 0
                                      select x;
                     list_drugs = _list_drugs.ToList();
                     
@@ -35,32 +35,32 @@ namespace Dap
                 else if ("2".Equals(status))//即将过期（标准为过期前两个月）
                 {
                     var _list_drugs = from x in dc.GetTable<Drugs>()
-                                     where x.OwnerId.ToString() == ownerId && x.RemainDay > 0 && x.RemainDay <= 60
+                                     where x.RemainDay == 60
                                      select x;
                     list_drugs = _list_drugs.ToList();
                     
                 }
-                else if("3".Equals(status))//其他药品
-                {
-                    var _list_drugs = from x in dc.GetTable<Drugs>()
-                                     where x.OwnerId.ToString() == ownerId && x.RemainDay > 60
-                                     select x;
-                    list_drugs = _list_drugs.ToList();
+                //else if("3".Equals(status))//其他药品
+                //{
+                //    var _list_drugs = from x in dc.GetTable<Drugs>()
+                //                     where x.OwnerId.ToString() == ownerId && x.RemainDay > 60
+                //                     select x;
+                //    list_drugs = _list_drugs.ToList();
 
-                }
+                //}
             }
             if(list_drugs != null)
             {
               
-                IOrderedEnumerable<Drugs> query;
+                //IOrderedEnumerable<Drugs> query;
 
-                query = list_drugs.OrderBy(c => c.RemainDay);
-                int pageSize = 8;
-                var results = query
-                              .Skip(pageSize * (page-1))
-                              .Take(pageSize)
-                              .ToList();
-                return results;
+                //query = list_drugs.OrderBy(c => c.RemainDay);
+                //int pageSize = 8;
+                //var results = query
+                //              .Skip(pageSize * (page-1))
+                //              .Take(pageSize)
+                //              .ToList();
+                return list_drugs;
             }
             else throw (new Exception("没有找过对应药品"));
 
--- DrugTest/Controllers/DrugsController.cs
+++ DrugTest/Controllers/DrugsController.cs
@@ -31,15 +31,15 @@ namespace DrugTest.Controllers
                 status = HttpContext.Current.Request.Form["status"];
                 ownerId = HttpContext.Current.Request.Form["ownerId"];
                 page = HttpContext.Current.Request.Form["page"];
-                if (null != status)
-                {
-                    result.result = Dap.drugs.getDrugsList(status, ownerId, int.Parse(page));
-                }
-                else
-                {
+                //if (null != status)
+                //{
+                //    result.result = Dap.drugs.getDrugsList(status, ownerId, int.Parse(page));
+                //}
+                //else
+                //{
                     result.result = Dap.drugs.getDrugsList(ownerId, int.Parse(page));
 
-                }
+                //}
                 
             }
             catch (Exception e)
