7b151ecea782bb25eb7a6bb218b8000c36f52c1e
cnbb12 <957971829@qq.com>
2019-02-17 11:35:19 +0800

    每隔一天检测RemainDay，执行推送

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,49 @@
-﻿using System;
+﻿using cn.jpush.api;
+using cn.jpush.api.push.mode;
+using cn.jpush.api.push.notification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
-using cn.jpush.api;
-using cn.jpush.api.push.mode;
-using cn.jpush.api.push.notification;
-using Dap;
-using Models;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
 
-namespace WebApplication1
+namespace ConsoleApp1
 {
-    public class Class1
+    class Program
     {
-        public static void Main()
+        static void Main(string[] args)
+        {
+           
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 24*60*60*1000; //执行间隔时间为一天
+            timer.Start();
+            timer.Elapsed += new ElapsedEventHandler(TimedEvent);
+            Console.ReadKey();
+
+        }
+
+        private static void TimedEvent(object source, ElapsedEventArgs e)
         {
             sendPush();
 
         }
         public static void sendPush()
         {
-            string AppKey = ConfigurationManager.AppSettings["AppKey"];
-            string MasterSecret = ConfigurationManager.AppSettings["MasterSecret"];
+            string AppKey = "6d39f5bdddfe1143a3d55895";
+            string MasterSecret = "74758e5fd26d7bb2faa74488";
             JPushClient client = new JPushClient(AppKey, MasterSecret);//根据app信息生成推送client
             List<PushPayload> pushPayLoads = JPushObjecWithExtrasAndMessage();//设置推送的具体参数
-            foreach(PushPayload pushPayLoad in pushPayLoads) {
+            foreach (PushPayload pushPayLoad in pushPayLoads)
+            {
                 var reslut = client.SendPush(pushPayLoad);//推送
                 var apiReslut = client.getReceivedApi(reslut.msg_id.ToString());//接收推送结果
             }
-               
-         
-       
+
+
+
 
         }
 
@@ -44,27 +57,25 @@ namespace WebApplication1
             List<PushPayload> pushes = new List<PushPayload>();
 
             //添加别名实现个推
-            List<Models.Drugs> list =  Dap.drugs.getDrugsList("1");//1:RemainDay==0
-           
-            foreach(Models.Drugs drug in list)
+            List<Models.Drugs> list = Dap.drugs.getDrugsList("1");//1:RemainDay==0
+
+            foreach (Models.Drugs drug in list)
             {
                 PushPayload pushPayload = new PushPayload();
                 pushPayload.platform = Platform.all();
                 string s = drug.OwnerId.ToString().Replace("-", "");//将userId去掉间隔符作为别名
                 pushPayload.audience = Audience.s_alias(s);
                 //通知
-                string alert = "你的药品" + drug.Name.ToString()+"已经过期";
+                string alert = "你的药品" + drug.Name.ToString() + "已经过期";
                 pushPayload.notification = new Notification().setAndroid(new AndroidNotification().setAlert(alert));
                 pushes.Add(pushPayload);
-                
+
             }
-          
+
             return pushes;
 
 
         }
     }
-
-
-
 }
+ 
